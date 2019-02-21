5b3b9c4963de187d9f44f381428afb6fff49cbc1
francois dutheil <frdutheil@yahoo.fr>
2019-02-21 17:19:44 +0100

    afternoon following

--- app/models/island.rb
+++ app/models/island.rb
@@ -2,6 +2,7 @@ class Island < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+  has_many :reviews, dependent: :destroy
   mount_uploader :photo, PhotoUploader
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_143619) do
+ActiveRecord::Schema.define(version: 2019_02_21_144450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2019_02_20_143619) do
     t.index ["user_id"], name: "index_islands_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "island_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["island_id"], name: "index_reviews_on_island_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -56,4 +64,5 @@ ActiveRecord::Schema.define(version: 2019_02_20_143619) do
   add_foreign_key "bookings", "islands"
   add_foreign_key "bookings", "users"
   add_foreign_key "islands", "users"
+  add_foreign_key "reviews", "islands"
 end
